0ce1a4cd6e07aae01457b6792de9750d3d524181
Obo Agboghidi <obo@Obos-MacBook-Pro.local>
2017-05-12 16:14:58 -0700

    4th commit

---
+++ youtube/mock_data.sql
@@ -0,0 +1,24 @@
+INSERT INTO youtube VALUES (1, 'strategize real-time technologies', 'Accounting', 'Insituto Universitario - Centro Latinoamericano de Economia Humana - IU Claeh', 67, '772 Macpherson Parkway');
+INSERT INTO youtube VALUES (2, 'orchestrate next-generation niches', 'Services', 'Kitami Institute of Technology', 95, '40 Morning Place');
+INSERT INTO youtube VALUES (3, 'recontextualize cutting-edge applications', 'Human Resources', 'Brigham Young University', 59, '3 Bartelt Park');
+INSERT INTO youtube VALUES (4, 'leverage web-enabled infrastructures', 'Support', 'St.Kliment Ohridski University', 18, '086 Jana Place');
+INSERT INTO youtube VALUES (5, 'synthesize enterprise infrastructures', 'Research and Development', 'University of the Free State', 5, '11 Upham Crossing');
+
+
+INSERT INTO reccomendation VALUES (1, 'deliver open-source niches', 1, 63);
+INSERT INTO reccomendation VALUES (2, 'whiteboard impactful platforms', 8, 518);
+INSERT INTO reccomendation VALUES (3, 'engineer impactful schemas', 3, 998);
+INSERT INTO reccomendation VALUES  (4, 'integrate leading-edge functionalities', 10, 269);
+INSERT INTO reccomendation VALUES  (5, 'optimize user-centric web services', 6, 918);
+
+INSERT INTO purchase VALUES (1, 'unleash end-to-end methodologies', '30042354145951', '5:53 AM');
+INSERT INTO purchase VALUES (2, 'leverage vertical solutions', '3552615423547844', '10:47 AM');
+INSERT INTO purchase VALUES (3, 'monetize cutting-edge bandwidth', '3555472515686560', '3:40 PM');
+INSERT INTO purchase VALUES (4, 'target interactive infrastructures', '5100172764841313', '2:06 AM');
+INSERT INTO purchase VALUES (5, 'engineer extensible supply-chains', '4917574227529201', '3:11 AM');
+
+INSERT INTO history_list VALUES (1, 'scale virtual action-items', 'incentivize user-centric bandwidth');
+INSERT INTO history_list VALUES (2, 'optimize value-added web-readiness', 'benchmark killer e-services');
+INSERT INTO history_list VALUES (3, 'visualize interactive content', 'mesh rich convergence');
+INSERT INTO history_list VALUES (4, 'whiteboard interactive relationships', 'extend wireless niches');
+INSERT INTO history_list VALUES (5, 'optimize revolutionary partnerships', 'integrate efficient convergence');
